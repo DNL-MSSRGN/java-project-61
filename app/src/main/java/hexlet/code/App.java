caf3914eef6be65bce45d4583eec2d08d5261671
DNL MSSRGN <msssrgn@gmail.com>
2024-02-17 19:36:25 +0300

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,10 @@
 package hexlet.code;
 
+import static hexlet.code.Cli.scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
+        scanner();
     }
 }
